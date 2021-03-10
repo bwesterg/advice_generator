8aa6185118b5fdad456c59531ce9488911b5779d
Kyle Davis <kyledavis@Kyles-iMac.local>
2021-03-10 10:28:35 -0700

    throaway-merge

--- frontend/user.js
+++ frontend/user.js
@@ -1,6 +1,16 @@
+console.log('dfoighdfg')
+
 const searchParams = new URLSearchParams(window.location.search)
 const id = searchParams.get('id')
 
-fetch(`http://localhost:3000/user/${id}`)
+fetch(`http://localhost:3000/users/${id}`)
+    .then(response => response.json())
+    .then(user => {
+        const $showUserSection = document.createElement('section')
+        $showUserSection.innerHTML = `
+            <h1>Welcome, ${user.username}. Do you need some advice today?</h1>
+        `
+        document.querySelector('body').append($showUserSection)
+    })
 
-console.log('dfoighdfg')
+    
